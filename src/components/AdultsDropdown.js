11b5c407d78c4746a91df2e9b3bc129839fe4527
David Gurovich <david.gurovich@outlook.com>
2023-03-28 15:52:19 -0700

    added drop down buttons

--- src/components/AdultsDropdown.js
+++ src/components/AdultsDropdown.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {RoomContext} from '../context/RoomContext';
+import {Menu} from '@headlessui/react';
+import {Bschev} from 'react-icons/bs';
+import { BsChevronDown } from 'react-icons/bs';
+
+const lis = [
+  {name: '1 Adult'},
+  {name: '2 Adult'},
+  {name: '3 Adult'},
+  {name: '4 Adult'},
+]
 
 const AdultsDropdown = () => {
-  return <div>AdultsDropdown</div>;
+  return <Menu as='div' className='w-full h-full bg-white relative'>
+    <Menu.Button className='w-full h-full flex items-center justify-between px-8'>
+      adults
+      </Menu.Button>
+    <BsChevronDown/>
+    <Menu.Items as='ul'>
+      {lis.map((li, index ) => {
+        return (<Menu.Item as='li' key={index}>
+          {li.name}
+          </Menu.Item>
+        );
+      })}
+    </Menu.Items>
+  </Menu>;
 };
 
 export default AdultsDropdown;
