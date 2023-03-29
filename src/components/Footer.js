2b6ec01fb2d931716aaaf7ba26efba32b7cf9ee1
David Gurovich <david.gurovich@outlook.com>
2023-03-29 15:35:34 -0700

    added a footer with logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import LogoWhite from '../assets/img/logosd.svg';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer className='bg-primary py-8'>
+    <div className='container mx-auto text-white flex justify-between items-center'>
+      <img src={LogoWhite} alt="" />
+      Copyright &copy; 2022. All rights reserved.
+    </div>
+  </footer>;
 };
 
 export default Footer;
--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -12,23 +12,31 @@ const slides = [
   {
     title: 'Your Luxury Hotel For Vacation',
     bg: Img1,
-    btnText: 'Room & Suites'
+    btnText: 'See our rooms'
   },
   {
     title: 'Your Luxury Hotel For Vacation',
     bg: Img2,
-    btnText: 'Room & Suites'
+    btnText: 'See our rooms'
   },
   {
     title: 'Your Luxury Hotel For Vacation',
     bg: Img3,
-    btnText: 'Room & Suites'
+    btnText: 'See our rooms'
   },
 ]
 
 const HeroSlider = () => {
   return (
-    <Swiper className='heroSlider h-[600px] lg:h-[860px] '>
+    <Swiper 
+    modules={[EffectFade, Autoplay]} 
+    effect={'fade'} 
+    loop={true}
+    autoplay={{
+      delay:3000,
+      disableOnInteraction: false,
+    }}
+    className='heroSlider h-[600px] lg:h-[860px] '>
       {slides.map((slide, index) => {
         const {title, bg, btnText} = slide
         return <SwiperSlide className='h-full bg-pink-300 relative flex justify-center items-center' key={index}>
@@ -36,6 +44,7 @@ const HeroSlider = () => {
             <div className='uppercase font-tertiary tracking-[6px] mb-5'>Just Enjoy and relax</div>
             <h1 className='text-[32] font-primary uppercase tracking-[2px] 
             max-w-[920px] lg:text-[68px] leading-tight'>{title}</h1>
+            <button className='btn btn-lg btn-primary mx-auto'>{btnText}</button>
           </div>
           <div className='absolute top-0 w-full h-full'>
             <img src={bg} 
