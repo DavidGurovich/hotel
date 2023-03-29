1bde50967fcaf1ec6a2a8363579aa761e071d6b6
David Gurovich <david.gurovich@outlook.com>
2023-03-29 15:13:05 -0700

    added sliding images to hero page

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,7 +1,52 @@
 import React from 'react';
 
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import {EffectFade, Autoplay} from 'swiper';
+import Img1 from '../assets/img/heroSlider/1.jpg';
+import Img2 from '../assets/img/heroSlider/2.jpg';
+import Img3 from '../assets/img/heroSlider/3.jpg';
+
+const slides = [
+  {
+    title: 'Your Luxury Hotel For Vacation',
+    bg: Img1,
+    btnText: 'Room & Suites'
+  },
+  {
+    title: 'Your Luxury Hotel For Vacation',
+    bg: Img2,
+    btnText: 'Room & Suites'
+  },
+  {
+    title: 'Your Luxury Hotel For Vacation',
+    bg: Img3,
+    btnText: 'Room & Suites'
+  },
+]
+
 const HeroSlider = () => {
-  return <div className='heroSlider h-[600px] lg:h-[860px] bg-black'>HeroSlider</div>;
+  return (
+    <Swiper className='heroSlider h-[600px] lg:h-[860px] '>
+      {slides.map((slide, index) => {
+        const {title, bg, btnText} = slide
+        return <SwiperSlide className='h-full bg-pink-300 relative flex justify-center items-center' key={index}>
+          <div className='z-20 text-white text-center'>
+            <div className='uppercase font-tertiary tracking-[6px] mb-5'>Just Enjoy and relax</div>
+            <h1 className='text-[32] font-primary uppercase tracking-[2px] 
+            max-w-[920px] lg:text-[68px] leading-tight'>{title}</h1>
+          </div>
+          <div className='absolute top-0 w-full h-full'>
+            <img src={bg} 
+            className='object-cover h-full w-full' 
+            alt="" />
+          </div>
+          <div className='absolute w-full h-full bg-black/70'></div>
+        </SwiperSlide>;
+      })}
+    </Swiper>
+  )
 };
 
 export default HeroSlider;
